155d52eea7938da8518aa50d7827e7ba78035d8a
Nik Osvalds <nosvalds@gmail.com>
2020-06-05 11:17:39 +0100

    addItems Test 2 complete

--- src/ToDo/reducer.test.js
+++ src/ToDo/reducer.test.js
@@ -13,9 +13,11 @@ it('adds items', () => {
     // Adding tests here
     let result = addItem(initialState, { value: "Hello" });
 
-    // first item in the items array should be a new task with "Hello"
+    // 01 first item in the items array should be a new task with "Hello"
     expect(result.items[0]).toEqual({ task: "Hello", completed: false });
 
+    // 02 shouldn't be the same array we started with
+    expect(result.items).not.toBe(initialState.items);
 });
 
 it('removes items', () => {
