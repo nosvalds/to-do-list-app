5200ef07a407939892d85e4ad3fd4a9c28aa6bed
Nik Osvalds <nosvalds@gmail.com>
2020-06-05 12:33:05 +0100

    completeItem Test 2+3 complete

--- src/ToDo/reducer.test.js
+++ src/ToDo/reducer.test.js
@@ -104,6 +104,15 @@ it('completes items', () => {
     
     // expected completed to be true
     expect(completed.items[1]).toEqual({ task: "Mum", completed: true });
+
+    // 02 check that it's not the same object being returned
+    expect(completed.items[1]).not.toBe(many.items[1]);
+
+    // 03 complete item at index 0
+    completed = completeItem(many, { index: 0 });
+
+    // should still be marked as completed
+    expect(completed.items[0]).toEqual({ task: "Hello", completed: true });
 });
 
 it('reduces', () => {
