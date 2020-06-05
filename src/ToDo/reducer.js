87a981dcb6ac0b7a432ed5449c9d3bab550e6c5e
Nik Osvalds <nosvalds@gmail.com>
2020-06-05 12:29:24 +0100

    updateItem Test 3 complete

--- src/ToDo/reducer.js
+++ src/ToDo/reducer.js
@@ -1,20 +1,38 @@
+const deepCopyFunction = (inObject) => {
+    let outObject, value, key
+  
+    if (typeof inObject !== "object" || inObject === null) {
+      return inObject // Return the value if inObject is not an object
+    }
+  
+    // Create an array or object to hold the values
+    outObject = Array.isArray(inObject) ? [] : {}
+  
+    for (key in inObject) {
+      value = inObject[key]
+  
+      // Recursively (deep) copy for nested objects, including arrays
+      outObject[key] = deepCopyFunction(value)
+    }
+  
+    return outObject
+  }
+
+
 export const addItem = (state, { value }) => {
-    let newState = {...state};
-    newState.items = [...state.items];
+    let newState = deepCopyFunction(state);
     newState.items.push({ task: value, completed: false });
     return newState
 };
 
 export const removeItem = (state, { index }) => {
-    let newState = {...state};
-    newState.items = [...state.items];
+    let newState = deepCopyFunction(state);
     newState.items.splice(index, 1);
     return newState;
 }
 
-export const updateItem = (state, { index, value}) => {
-    let newState = {...state};
-    newState.items = [...state.items];
+export const updateItem = (state, { index, value }) => {
+    let newState = deepCopyFunction(state);
     newState.items[index].task = value;
     return newState;
 }
