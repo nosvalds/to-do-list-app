741d6c66abbabb4bb27bb894b8a9a015bc8b3ca6
Nik Osvalds <nosvalds@gmail.com>
2020-06-05 14:03:39 +0100

    refactor addItem to correctly use spread

--- src/ToDo/reducer.js
+++ src/ToDo/reducer.js
@@ -1,8 +1,12 @@
 export const addItem = (state, { value }) => {
-    let newState = {...state};
-    newState.items = [...state.items];
-    newState.items.push({ task: value, completed: false });
-    return newState
+    return {...state,
+                    items: [...state.items,
+                        { task: value, completed: false }
+                    ]
+    };
+    // newState.items = ;
+    // newState.items.push({ task: value, completed: false });
+    // return newState
 };
 
 export const removeItem = (state, { index }) => {
